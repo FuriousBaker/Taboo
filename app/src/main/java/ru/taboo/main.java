9275e0c287421bc1f425c83ccaa5d47c209968c7
FuriousBaker <FuriousBaker@gmail.com>
2015-01-11 16:27:16 +0400

    Промежуточные измениня?

--- app/src/main/java/ru/taboo/main.java
+++ app/src/main/java/ru/taboo/main.java
@@ -4,15 +4,30 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import static android.view.View.*;
 
 
-public class main extends ActionBarActivity {
+public class main extends ActionBarActivity implements OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+        Button btnStart = (Button) findViewById(R.id.btnStart);
+        Button btnCards = (Button) findViewById(R.id.btnCards);
+        Button btnPref = (Button) findViewById(R.id.btnPreferences);
+        Button btnRules = (Button) findViewById(R.id.btnRules);
+        btnStart.setOnClickListener(this);
+        btnCards.setOnClickListener(this);
+        btnPref.setOnClickListener(this);
+        btnRules.setOnClickListener(this);
     }
+
+
 
 
     @Override
